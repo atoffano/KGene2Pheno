d501dbd40983206f803b555188050d6bcebfe7b7
antoine <antoine@univ-rennes1.fr>
2023-02-15 14:35:40 +0100

    anyburl config

--- main.py
+++ main.py
@@ -15,21 +15,8 @@ def main():
     parser.add_argument('--ouput', default='./', help='Directory to store the data')
 
     #AnyBURL specific arguments
-    parser.add_argument('--snapshots_at', nargs='*', default=100, help='Rule snapshots at which timestamps (s). Usually decent predictions at 1000, may need to be higher for larger datasets.')
     parser.add_argument('--mem', default=4, help='Memory to use (GB)')
-    parser.add_argument('--threshold_correct_predictions', default=5, help='Threshold for correct predictions')
-    parser.add_argument('--batch_time', default=2000, help='time for batch')
-    parser.add_argument('--worker_threads', default=20, help='how many workers to use')
-    parser.add_argument('--policy', default=2, help='Possible values are 1 (greedy policy) and 2 (weighted policy). Experiments do not show a significant difference even tough the weighted variant is probably more robust in the sense that its less negative affected by the specifics of rather specific datasets.')
-    parser.add_argument('--reward', default=5, help='Possible values are 1 (correct predictions), 3 (correct predictions weighted by confidence with laplace smoothing), 5 (correct predictions weighted by confidence with laplace smoothing divided by (rule length-1)^2).')
-    parser.add_argument('--epsilon', default=0.1, help='0.1 is the default setting, which allocates a core with a probability of 0.1 randomly. You can change this to a value of 0.0 to 1.0 (= random policy).')
-    # Length of the rules
-    parser.add_argument('--max_length_cyclic', default=3, help='called binary rules or cyclic rules')
-    parser.add_argument('--zero_rules_active', default=False, help='rules that try to capture very simple relation specific frequencies')
-    parser.add_argument('--max_length_acyclic', default=1, help='these rules are called AC1 and AC2 rule or U_c or U_d rules (unary)')
-    parser.add_argument('--max_length_grounded_cyclic', default=1, help='U_d or AC1 rules that are derived from a closed path')
-
-
+    
     args = parser.parse_args()
 
     # Change directory
@@ -74,7 +61,24 @@ def train_model(method, dataset):
     elif method == "HybridGNN":
         pass
     elif method == "AnyBURL":
-        pass
+        import subprocess
+
+        # Check data file validity
+        with open(dataset, 'r') as f:
+            lines = f.readlines()
+            for line in lines:
+                if not str.isalpha(line[0]):
+                    raise Exception("AnyBURL expects that each identifier that appears in the data file starts with a alphabetic character and consists of at least 2 letters.")
+        
+        # Add training path to config file
+        with open('AnyBURL/config-learn.properties', 'a') as f:
+            lines = f.readlines()
+            f.write(f'PATH_TRAINING = {dataset}')
+        
+        # Remove the changes made to the config file
+        with open('AnyBURL/config-learn.properties', 'w') as f:
+            f.write(lines)
+
     elif method == "GraphLP":
         pass
 
