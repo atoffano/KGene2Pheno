81298a4ba98cdc6135bb6baf3d652821ba3aa8a9
antoine <antoine@univ-rennes1.fr>
2023-04-17 16:16:52 +0200

    gen_scores

--- run.sh
+++ run.sh
@@ -14,6 +14,7 @@ JOBNAME='ConvKB'
 #SBATCH --hint=nomultithread # desactiver l’hyperthreading
 
 export WANDB_API_KEY=4e5748d6c6f3917c78cdc38a516a1bac776faf58
+
 methods=("ConvKB")
 for method in "${methods[@]}"; do
     for dist in 'L1' 'L2'; do
@@ -21,8 +22,7 @@ for method in "${methods[@]}"; do
             for epoch in 5 10 20; do
                 for batch_size in 10230 8184 6138 4092; do
                     for n_filters in 5 10 25 50; do
-                            set -x # activer l’echo des commandes
-                            srun python /home/antoine/gene_pheno_pred/main.py --keywords gene phenotype interaction disease_plus_ortho disease-ontology phenotype-ontology expression_pattern --method $method --dataset local_celegans --n_epochs $epoch --batch_size $batch_size --lr 0.001 --normalize_parameters --loss_fn $loss --ent_emb_dim 50 --split_ratio 0.8 --dissimilarity_type $dist --margin 1 --n_filters $n_filters --save_model --save_data --init_transe True
+                            python /home/antoine/gene_pheno_pred/main.py --keywords gene phenotype interaction disease_plus_ortho disease-ontology phenotype-ontology expression_pattern --method $method --dataset local_celegans --n_epochs $epoch --batch_size $batch_size --lr 0.001 --normalize_parameters --loss_fn $loss --ent_emb_dim 50 --split_ratio 0.8 --dissimilarity_type $dist --margin 1 --n_filters $n_filters --save_model --save_data --init_transe True
                             wait
                         done
                     done
@@ -31,6 +31,25 @@ for method in "${methods[@]}"; do
         done
     done
 done
+
+
+# methods=("ConvKB")
+# for method in "${methods[@]}"; do
+#     for dist in 'L1' 'L2'; do
+#         for loss in "logistic" "margin"; do
+#             for epoch in 5 10 20; do
+#                 for batch_size in 10230 8184 6138 4092; do
+#                     for n_filters in 5 10 25 50; do
+#                             set -x # activer l’echo des commandes
+#                             srun python /home/antoine/gene_pheno_pred/main.py --keywords gene phenotype interaction disease_plus_ortho disease-ontology phenotype-ontology expression_pattern --method $method --dataset local_celegans --n_epochs $epoch --batch_size $batch_size --lr 0.001 --normalize_parameters --loss_fn $loss --ent_emb_dim 50 --split_ratio 0.8 --dissimilarity_type $dist --margin 1 --n_filters $n_filters --save_model --save_data --init_transe True
+#                             wait
+#                         done
+#                     done
+#                 done
+#             done
+#         done
+#     done
+# done
 
 
 
